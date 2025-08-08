b34b68db02c27ab9641cde11d51538e475f3e23d
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-08-08 17:07:37 +0300

    deployments and production

--- next.config.js
+++ next.config.js
@@ -2,11 +2,18 @@ module.exports = {
   images: {
     remotePatterns: [
       {
+        protocol: 'https',
+        hostname: 'felikz97.pythonanywhere.com',
+        port: '',
+        pathname: '/media/**',
+      },
+      // Optional: keep localhost for local dev
+      {
         protocol: 'http',
         hostname: 'localhost',
         port: '8000',
         pathname: '/media/**',
       },
     ],
-  }
+  },
 }
